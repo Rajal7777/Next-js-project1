ca4fe8fcb0c6f7d534064f643602715169656d86
Rajal7777 <suwalrajal57@gmail.com>
2026-05-20 19:59:26 +0900

    added the layout for product list and categories

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,17 @@
-export default function HomePage(){
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
+
+export default function HomePage() {
   return (
-    <div>Home page</div>
-  )
+    <div>
+      {/* aspect-3/1 ->  width-3 and height-1 of the width */}
+      <div className="relative aspect-3/1 mb-12">
+        {/* using Image the parent container must have the position relative not static and must give the size while using the fill property */}
+        <Image src='/featured.png' alt="Product banner photo" fill />
+      </div>
+      <ProductList />
+
+    </div>
+  );
 }
